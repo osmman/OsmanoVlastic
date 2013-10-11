d21798506a4830129f0cfced7a66cf5a822e5f16
Bc. Vlastimil Jinoch <jinocvla@fel.cvut.cz>
2013-10-11 15:30:48 +0200

    Add js start part.

---
+++ src/main/webapp/assets/js/authentication.js
@@ -0,0 +1,40 @@
+/*
+ * Authentication method provide Sign in and generating HTTP Basic
+ * 
+ */
+
+Authentication = {
+	signInSubmit : function(e) {
+		if ($('#authenticationName').val().length == 0 || $('#authenticationName').val().length == 0) {
+			return;
+		}
+		Authentication.signIn($('#authenticationName').val(), $(
+				'#authenticationPassword').val());
+		$('#authenticationName').val("");
+		$('#authenticationPassword').val("");
+		$('#signIn').hide();
+		$('#signOut').show();
+		$('#authenticationSignetUser').text(Authentication.name);
+		$('#body').show();
+		$('#body-unautorized').hide();
+		e.preventDefault();
+	},
+	signOutSubmit : function() {
+		Authentication.signOut();
+		$('#signIn').show();
+		$('#signOut').hide();
+		$('#authenticationSignetUser').text('');
+		$('#body').hide();
+		$('#body-unautorized').show();
+	},
+	signIn : function(name, password) {
+		Authentication.name = name;
+		Authentication.token = Base64.encode(Authentication.name+":"+Authentication.password);
+	},
+	signOut : function() {
+		Authentication.name = "";
+		Authentication.token = "";
+	},
+	name : "",
+	token : ""
+}
