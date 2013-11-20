d5570446e947b184d82896bb16f9da1ee88c1995
Bc. Tomáš Turek <turekto5@fel.cvut.cz>
2013-11-20 13:13:32 +0100

    Uprava ReservationMapper pro vstup Flight id.

--- src/main/java/core/mapper/ReservationMapper.java
+++ src/main/java/core/mapper/ReservationMapper.java
@@ -1,26 +1,45 @@
 package core.mapper;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
-import model.Destination;
 import model.Flight;
 import model.Reservation;
 
+import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name="reservation")
-public class ReservationMapper extends Mapper<Reservation> {
-	private Integer seats;
-	
-	public Integer getSeats() {
-		return seats;
-	}
-	public void setSeats(Integer seats) {
-		this.seats = seats;
-	}
-	
-	public Reservation map(Reservation origin) {
-		origin.setSeats(seats);
-		return origin;
-	}
-	
+
+@XmlRootElement(name = "reservation")
+public class ReservationMapper extends Mapper<Reservation>
+{
+    private Integer seats;
+
+    private Long flight;
+
+    public Integer getSeats()
+    {
+        return seats;
+    }
+
+    public void setSeats(Integer seats)
+    {
+        this.seats = seats;
+    }
+
+    public Long getFlight()
+    {
+        return flight;
+    }
+
+    public void setFlight(Long flight)
+    {
+        this.flight = flight;
+    }
+
+    public Reservation map(Reservation origin)
+    {
+        origin.setSeats(seats);
+        Flight flight = new Flight();
+        flight.setId(this.flight);
+        origin.setFlight(flight);
+        return origin;
+    }
+
 }
