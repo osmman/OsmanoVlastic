248c72d3a10ba47ed8f87c1ce8d4f26696769d5f
Bc. Tomáš Turek <turekto5@fel.cvut.cz>
2013-11-20 14:13:56 +0100

    import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,17 @@
-insert into Destination (id,name) values (HIBERNATE_SEQUENCE.nextval,'New Yourk'); 
-insert into Destination (id,name) values (HIBERNATE_SEQUENCE.nextval,'Prague'); 
-insert into Destination (id,name) values (HIBERNATE_SEQUENCE.nextval,'Paris'); 
-insert into Destination (id,name) values (HIBERNATE_SEQUENCE.nextval,'Tokyo'); 
-insert into Destination (id,name) values (HIBERNATE_SEQUENCE.nextval,'London');
+SET @ny = HIBERNATE_SEQUENCE.nextval;
+insert into Destination (id,name,latitude,longitude) values (@ny,'New Yourk',40.7143528,-74.00597309999999);
 
-insert into Flight (id, name, from_id, to_id, dateOfDeparture, seats) values (HIBERNATE_SEQUENCE.nextval, 'prag-ny', 2,1, '2013-07-08 02:00:00.0', 15);
+SET @prag = HIBERNATE_SEQUENCE.nextval;
+insert into Destination (id,name,latitude,longitude) values (@prag,'Prague',50.0755381,14.4378005);
+
+
+insert into Destination (id,name,latitude,longitude) values (HIBERNATE_SEQUENCE.nextval,'Paris',48.856614,2.3522219);
+insert into Destination (id,name,latitude,longitude) values (HIBERNATE_SEQUENCE.nextval,'Tokyo',35.6894875,139.6917064);
+insert into Destination (id,name,latitude,longitude) values (HIBERNATE_SEQUENCE.nextval,'London',51.51121389999999,-0.1198244);
+
+SET @flight = HIBERNATE_SEQUENCE.nextval;
+insert into Flight (id, name, from_id, to_id, dateOfDeparture, seats, distance, price) values (@flight, 'prag-ny', @prag,@ny, '2013-07-08 02:00:00.0', 15,6572.01806640625,65720.1796875);
+
+
+
+insert into Reservation (id, created, password, seats, state, flight_id) values (HIBERNATE_SEQUENCE.nextval, NOW(), 'il1jkjq41wgplcosozi1t25ouq5ruezp', 10, 'PAID', @flight);
