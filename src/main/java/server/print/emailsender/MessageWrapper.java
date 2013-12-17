e6c713d2a509e946105feae7b0168ef31c3d39aa
Bc. Vlastimil Jinoch <jinocvla@fel.cvut.cz>
2013-12-17 12:59:44 +0100

    Update message wrapper.

--- src/main/java/server/print/emailsender/MessageWrapper.java
+++ src/main/java/server/print/emailsender/MessageWrapper.java
@@ -1,6 +1,7 @@
 package server.print.emailsender;
 
 
+import java.io.File;
 import java.io.Serializable;
 
 /**
@@ -11,13 +12,16 @@ public class MessageWrapper implements Serializable
 
     private String email;
 
+    private File file;
+
     public MessageWrapper()
     {
     }
 
-    public MessageWrapper(String email)
+    public MessageWrapper(String email, File file)
     {
         this.email = email;
+        this.file = file;
     }
 
     public String getEmail()
@@ -28,5 +32,14 @@ public class MessageWrapper implements Serializable
     public void setEmail(String email)
     {
         this.email = email;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+
+        return file;
     }
 }
